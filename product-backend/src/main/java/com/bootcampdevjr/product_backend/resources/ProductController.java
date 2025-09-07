7b60efac4a18de0462c06c6af12f7f055047f781
Nickolas Machado <nickolasraphael3232@gmail.com>
2025-09-07 19:29:56 -0300

    added frontend

---
+++ product-backend/src/main/java/com/bootcampdevjr/product_backend/resources/ProductController.java
@@ -0,0 +1,42 @@
+package com.bootcampdevjr.product_backend.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.bootcampdevjr.product_backend.models.Product;
+
+
+@RestController
+public class ProductController {
+
+    // int id, String name, String description, int idCategory, boolean promotion, boolean newProduct, double price
+
+    private List<Product> products = Arrays.asList( 
+        new Product(1, "Product 01", "Description 01", 1, false, false, 100.50),
+        new Product(2, "Product 02", "Description 02", 2, true, true, 200.50),
+        new Product(3, "Product 03", "Description 03", 3, false, false, 300.50));
+
+    @GetMapping("products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+        Product prod = products.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        return ResponseEntity.ok(prod);
+
+    }
+
+    @GetMapping("products")
+    public List<Product> getProducts() {
+        return products;
+    }
+}
